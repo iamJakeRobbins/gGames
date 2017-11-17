b2d644428d9e4e43ea28cdd42497d994defd0c18
Jake Robbins <jrobbinsd@gmail.com>
2017-11-17 08:29:25 -0700

    bugfixin, can actually add games to gamers

--- routes/gamers/gamers.js
+++ routes/gamers/gamers.js
@@ -19,47 +19,55 @@ router.get('/:id', (req, res) =>{
 	knex('gamer')
 	.select()
 	.where('gamer.id', req.params.id)
-	.leftJoin('gamer_game', 'gamer.id', 'gamer_id')
-	.leftJoin('game', 'game_id', 'game.id')
-	.then(gamer =>{
-		let gamesData = []
-		let gameObj = new Object()
-		for (var i = 0; i < gamer.length; i++) {
-			gameObj = new Object();
-			gameObj.title = gamer[i].title
-			gamesData.push(gameObj)
-		}
-		res.render('gamers/single', {gamer:gamer[0], gamesData:gamesData, games:games})
+	.innerJoin('gamer_game', 'gamer.id', 'gamer_id')
+	.innerJoin('game', 'game_id', 'game.id')
+	.then(gamesData =>{
+		knex('gamer').where('id', req.params.id)
+		.then(gamerData =>{
+			let data = {
+				gamer: gamerData[0],
+				gamesData: gamesData,
+				games: games,
+			}
+			console.log(data);
+			res.render('gamers/single',data)
+			})
 		})
 	})
 });
 
 
-// router.post('/', (req, res) =>{
-// 	knex('gamer')
-// 	.insert({
-// 		name:req.body.name,
-// 	})
-// 	.then( () =>{
-// 		res.redirect('gamers')
-// 	})
-// })
+router.post('/', (req, res) =>{
+	knex('gamer')
+	.insert({
+		name:req.body.name,
+	})
+	.then( () =>{
+		res.redirect('gamers')
+	})
+})
 
 router.post('/:id', (req, res) =>{
-	// console.log(req.params.id);
 	req.body.gamer_id = req.params.id
-	console.log(req.body);
 	knex('gamer_game')
 	.insert({
 		gamer_id: req.body.gamer_id,
 		game_id: req.body.game_id
 	})
-	// console.log(req.body)
-	.then( (stuff) =>{
-
+	.then( () =>{
 		res.redirect(`/gamers/${req.body.gamer_id}`)
 	})
 })
+
+// router.delete('/:id', (req, res) =>{
+// 	console.log('hello')
+// 	knex('gamer_game')
+// 	.where('id', req.body.id)
+// 	.del()
+// 	.then(() =>{
+// 		res.redirect(`/gamers/${req.body.gamer_id}`)
+// 	})
+// })
 
 
 module.exports = router;
